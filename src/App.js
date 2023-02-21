36203c2386f473578e4fa3d1c71cb13475241346
Yehor <malovanyiyehor@icloud.com>
2023-02-21 14:10:37 +0100

    adding tg

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 
+const tg = window.Telegram.WebApp
+
 function App() {
   useEffect(() => {
     tg.ready()
